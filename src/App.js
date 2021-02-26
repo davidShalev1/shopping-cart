55bd925e3d7f60f317bef54a3c05988c84f0ee73
david shalev <davidshalev1234@gmail.com>
2021-02-26 19:15:35 +0200

    add some style

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="grid-container">
+        <header>
+          <a href="/">Shopping-cart</a>
+        </header>
+        <main>
+          Product List
+        </main>
+        <footer>
+         all right is reserved 
+        </footer>
+    </div>
+  );
+}
+
+export default App;
